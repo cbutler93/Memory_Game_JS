37dd67c4ddf7455bd0fc3fd2d3556e47acda55c7
CButler93 <chrisbutlerdev@gmail.com>
2018-09-12 12:12:09 -0600

    style: Reformat code with 4 spaces instead of indents. Moved initiation/reset functions to the top of the JS file.

--- js/app.js
+++ js/app.js
@@ -10,87 +10,45 @@ const gameStats = document.querySelector('.finished');
 
 // Scramble initialized cards array
 function randomizeArray(arr) {
-     const randIndices = []; // Array to hold random indices selected.
-     const randArray = []; // Array to hold elements from random array indices.
-     while (randArray.length < 16) {
-          const randIndex = Math.floor(Math.random() * arr.length);
-          if (randIndices.includes(randIndex)) {
-               continue;
-          } else {
-               randIndices.push(randIndex)
-               randArray.push(arr[randIndex]);
-          }
-     }
-     return randArray;
-};
+    const randIndices = []; // Array to hold random indices selected.
+    const randArray = []; // Array to hold elements from random array indices.
+    while (randArray.length < 16) {
+        const randIndex = Math.floor(Math.random() * arr.length);
+        if (randIndices.includes(randIndex)) {
+            continue;
+        } else {
+            randIndices.push(randIndex)
+            randArray.push(arr[randIndex]);
+        }
+    }
+    return randArray;
+}
 
 // Create a new deck of cards for a new game.
 function initCards() {
-     // Initialize Card Array
-     const initCards = [
-          'fa-diamond', 'fa-diamond',
-          'fa-paper-plane-o', 'fa-paper-plane-o',
-          'fa-anchor', 'fa-anchor',
-          'fa-bolt', 'fa-bolt',
-          'fa-cube', 'fa-cube',
-          'fa-leaf', 'fa-leaf',
-          'fa-bicycle', 'fa-bicycle',
-          'fa-bomb', 'fa-bomb'
-     ];
-     // Randomize cards for start of game.
-     const newCards = randomizeArray(initCards);
-     // Initialize game by hiding all cards and adding a random icon to each.
-     const cardElements = document.querySelectorAll('.card');
-     cardElements.forEach(el => el.classList.remove('open', 'match', 'show'));
-     const cardSymbols = document.querySelectorAll('.card i');
-     cardSymbols.forEach(el => {
-         const curIcon = el.classList[1];
-         el.classList.remove(curIcon);
-         const newIcon = newCards.pop();
-         el.classList.add(newIcon);
-     });
-};
-
-function revealCard(el) {
-    el.classList.add('open', 'show');
-};
-
-function hideCards(...cards) {
-    for (const card of cards) {
-        card.classList.remove('open', 'show', 'wrong');
-        card.style.animationName = 'none';
-    }
-};
-
-function checkGameOver(matches) {
-    if (matches === 8) { // There are 16 cards, 8 two-card matches means all cards matched.
-        gameOver = true;
-        gameStats.style.display = 'inline-block';
-        gameStats.classList.add('slide-in');
-        const timeStats = document.querySelector('.total-time');
-        const starsEarned = document.querySelector('.star-rating');
-        timeStats.textContent = `It took you ${totalTime} to complete the game!`;
-        starsEarned.textContent = `You finished the game with a ${starRating}-star rating!`;
-        clearInterval(timer); // Stop timer.
-    }
-};
-
-function setClock() {
-    const startMessage = document.querySelector('.start');
-    startMessage.classList.add('shrink'); // Get rid of 'Select card to start' element.
-    const startTime = new Date().getTime();
-    const minuteSpan = document.querySelector('span.minutes');
-    const secondSpan = document.querySelector('span.seconds');
-    const timer = setInterval(() => {
-        const curTime = new Date().getTime();
-        const totalSeconds = ((curTime - startTime) / 1000).toFixed(0);
-        const seconds = totalSeconds < 60 ? totalSeconds : totalSeconds % 60;
-        const minutes = Math.floor(totalSeconds / 60);
-        secondSpan.textContent = seconds < 10 ? `0${seconds}` : `${seconds}`;
-        minuteSpan.textContent = minutes < 10 ? `0${minutes}` : `${minutes}`;
-        totalTime = `${minutes} ${minutes !== 1 ? `Minutes` : `Minute`} and ${seconds} ${seconds !== 1 ? `Seconds` : `Second`}`;
-    }, 1000);
-    return timer; // return timer so it can be stopped when the game is over or reset.
+    // Initialize Card Array
+    const initCards = [
+        'fa-diamond', 'fa-diamond',
+        'fa-paper-plane-o', 'fa-paper-plane-o',
+        'fa-anchor', 'fa-anchor',
+        'fa-bolt', 'fa-bolt',
+        'fa-cube', 'fa-cube',
+        'fa-leaf', 'fa-leaf',
+        'fa-bicycle', 'fa-bicycle',
+        'fa-bomb', 'fa-bomb'
+    ];
+    // Randomize cards for start of game.
+    const newCards = randomizeArray(initCards);
+    // Initialize game by hiding all cards and adding a random icon to each.
+    const cardElements = document.querySelectorAll('.card');
+    cardElements.forEach(el => el.classList.remove('open', 'match', 'show'));
+    const cardSymbols = document.querySelectorAll('.card i');
+    cardSymbols.forEach(el => {
+        const curIcon = el.classList[1];
+        el.classList.remove(curIcon);
+        const newIcon = newCards.pop();
+        el.classList.add(newIcon);
+    });
 }
 
 function resetClock() {
@@ -112,6 +70,48 @@ function resetGame() {
     resetClock();
     gameStats.style.display = 'none';
     gameStats.classList.remove('slide-in');
+}
+
+function revealCard(el) {
+    el.classList.add('open', 'show');
+}
+
+function hideCards(...cards) {
+    for (const card of cards) {
+        card.classList.remove('open', 'show', 'wrong');
+        card.style.animationName = 'none';
+    }
+}
+
+function checkGameOver(matches) {
+    if (matches === 8) { // There are 16 cards, 8 two-card matches means all cards matched.
+        gameOver = true;
+        gameStats.style.display = 'inline-block';
+        gameStats.classList.add('slide-in');
+        const timeStats = document.querySelector('.total-time');
+        const starsEarned = document.querySelector('.star-rating');
+        timeStats.textContent = `It took you ${totalTime} to complete the game!`;
+        starsEarned.textContent = `You finished the game with a ${starRating}-star rating!`;
+        clearInterval(timer); // Stop timer.
+    }
+}
+
+function setClock() {
+    const startMessage = document.querySelector('.start');
+    startMessage.classList.add('shrink'); // Get rid of 'Select card to start' element.
+    const startTime = new Date().getTime();
+    const minuteSpan = document.querySelector('span.minutes');
+    const secondSpan = document.querySelector('span.seconds');
+    const timer = setInterval(() => {
+        const curTime = new Date().getTime();
+        const totalSeconds = ((curTime - startTime) / 1000).toFixed(0);
+        const seconds = totalSeconds < 60 ? totalSeconds : totalSeconds % 60;
+        const minutes = Math.floor(totalSeconds / 60);
+        secondSpan.textContent = seconds < 10 ? `0${seconds}` : `${seconds}`;
+        minuteSpan.textContent = minutes < 10 ? `0${minutes}` : `${minutes}`;
+        totalTime = `${minutes} ${minutes !== 1 ? `Minutes` : `Minute`} and ${seconds} ${seconds !== 1 ? `Seconds` : `Second`}`;
+    }, 1000);
+    return timer; // return timer so it can be stopped when the game is over or reset.
 }
 
 function checkCards(cards) {
@@ -138,7 +138,7 @@ function checkCards(cards) {
         }
         setTimeout(hideCards, 1000, firstCard, secondCard);
     }
-};
+}
 
 function updateMoveDisplay() {
     const moves = document.querySelector('.moves');
@@ -161,7 +161,7 @@ function updateMoveDisplay() {
             lostStars.forEach(star => star.classList.replace('fa-star-o', 'fa-star'));
         }
     }
-};
+}
 
 // Add event listener for card selections.
 deck.addEventListener('click', (e) => {
